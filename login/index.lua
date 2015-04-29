5e105b858281b616a33ca24516a507260fde7eb5
Alexander F Rødseth <rodseth@gmail.com>
2015-04-29 11:22:12 +0200

    Further work on the login page

--- login/index.lua
+++ login/index.lua
@@ -1,7 +1,27 @@
-if method() == "POST" then
-  serve("post.amber")
-elseif method() == "GET" then
-  serve("get.amber")
+-- For retrieving parameters as part of the URL, specific Go methods
+-- must be used. Look at FormValues etc in pkg/net/http
+
+function mysplit(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={} ; i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
+end
+
+if method() == "GET" then
+  serve("index.amber")
+elseif method() == "POST" then
+  content("text/html")
+  print("GOT POST")
+  print(urlpath())
+  -- get the part after # with one of the pkg/net/http methods!
+  print(mysplit(urlpath(), "#")[2])
+  for key,value in pairs(formdata()) do print(key, value) end
 end
 
 -- vim:set ts=2 sw=2 et:
