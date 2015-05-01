97884fedca57ed13cfba662b9d6d3e728eee6056
Alexander F Rødseth <rodseth@gmail.com>
2015-05-01 14:06:49 +0200

    Login functionality

--- login/index.lua
+++ login/index.lua
@@ -1,27 +1,20 @@
--- For retrieving parameters as part of the URL, specific Go methods
--- must be used. Look at FormValues etc in pkg/net/http
-
-function mysplit(inputstr, sep)
-        if sep == nil then
-                sep = "%s"
-        end
-        local t={} ; i=1
-        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-                t[i] = str
-                i = i + 1
-        end
-        return t
-end
-
 if method() == "GET" then
   serve("index.amber")
 elseif method() == "POST" then
   content("text/html")
-  print("GOT POST")
-  print(urlpath())
-  -- get the part after # with one of the pkg/net/http methods!
-  print(mysplit(urlpath(), "#")[2])
-  for key,value in pairs(formdata()) do print(key, value) end
+  local data = formdata()
+  local username = data["username"]
+  local password = data["password"]
+  if HasUser(username) and CorrectPassword(username, password) then
+    print("Correct username and password.")
+    if CookieLogin(username) then
+      print("User was logged in with a cookie.")
+    else
+      print("Could not set login cookie!")
+    end
+  else
+    print("Incorrect username and/or password.")
+  end
 end
 
 -- vim:set ts=2 sw=2 et:
